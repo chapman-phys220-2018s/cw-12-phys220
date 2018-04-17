2b60dbaff05d4fd67eac36cf8f62445d45092a80
Myranda Hoggatt <hogga102@mail.chapman.edu>
2018-04-17 20:35:58 +0000

    added py files

---
+++ sombrero.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+###
+#Daniel Chang
+#2260161
+#Chang254@mail.chapman.edu
+#Myranda Hoggatt
+#2285495
+#hogga102@mail.chapman.edu
+#Devon Ball
+#2313078
+#dball@chapman.edu
+#PHYS220 Spring 2018
+#CW12
+###
+
